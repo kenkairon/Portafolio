6055839a331f2bdfaa9ea98ea7450a851b3a30f8
kenkairon <ingeinfo2006@gmail.com>
2025-07-14 12:07:00 -0400

    Cambios en certificaciones y plantilla curriculum

--- certificaciones/forms.py
+++ certificaciones/forms.py
@@ -2,13 +2,26 @@ from django import forms
 from .models import Certificacion
 
 class CertificacionForm(forms.ModelForm):
+    fecha_inicio = forms.DateField(
+        required=False,
+        widget=forms.DateInput(
+            attrs={'type': 'date', 'class': 'form-control'},
+            format='%Y-%m-%d'  
+        )
+    )
+    fecha_finalizacion = forms.DateField(
+        required=False,
+        widget=forms.DateInput(
+            attrs={'type': 'date', 'class': 'form-control'},
+            format='%Y-%m-%d'
+        )
+    )
+
     class Meta:
         model = Certificacion
         exclude = ['usuario']
         widgets = {
-            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre de la certificación'}),
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
             'imagen': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
-            'enlace': forms.URLInput(attrs={'class': 'form-control', 'placeholder': 'Enlace a la certificación (opcional)'}),
-            'fecha_inicio': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'fecha_finalizacion': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'enlace': forms.URLInput(attrs={'class': 'form-control'}),
         }
