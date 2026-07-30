b1ffe1c5b266dc46e57832353806d8b82143207e
kenkairon <ingeinfo2006@gmail.com>
2026-07-29 22:59:56 -0400

    Cambios

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ia6-*jz&22+39s7y4870131l)su71$e0i81@8nekdu&fjde4_j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'portafolio-personal0074.onrender.com']
 
 
 # Application definition
